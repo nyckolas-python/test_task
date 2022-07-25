fd393ed5c89ab4e3468537214009bf523a340e34
nyckolas <nyckolas.work@gmail.com>
2022-07-25 17:33:02 +0300

    Add SQL functions

--- SGM/app.py
+++ SGM/app.py
@@ -6,7 +6,7 @@ import pandas as pd
 import server
 import exceptions
 
-json_input = {"code1": "shA", "code2": "W"}
+json_input = {"code1": "shC", "code2": "C"}
 
 json_input_list = [{"code1": "shA", "code2": "W"},
                 {"code1": "shA", "code2": "S"},
@@ -19,35 +19,54 @@ json_input_list = [{"code1": "shA", "code2": "W"},
                 {"code1": "shC", "code2": "C"}]
 
 
-
 def csv_data(json_input):
-        try:
-            u_path = input("Enter the path and filename of press Enter if 'db_table.csv'-> ") # open the dialog
-            # 'db_table.csv' by default
-            u_path = 'db_table.csv' if u_path == '' else u_path
-            """Validating json_input by regular expression."""
-            server.check_json(json_input)
-            # this block can be removed, made to check the list of json
-            if type(json_input) == list:
-                input_df = pd.DataFrame.from_records(json_input, )
-            else:
-                input_df = pd.DataFrame([json_input])
-            df = pd.read_csv(u_path)
-            """Add new column 'bitCode', 'siCode' and add data to DataFrame"""
-            result = server.add_columns(df)
-            
-            result = server.filter_data(input_df, df)
-            
-            server.ask_output(result)
-            return result
+    try:
+        u_path = input("Enter the path and filename of press Enter if 'db_table.csv'-> ") # open the dialog
+        # 'db_table.csv' by default
+        u_path = 'db_table.csv' if u_path == '' else u_path
+        """Validating json_input by regular expression."""
+        json_input = server.check_json(json_input)
 
-        except Exception as e:
-                """exceptions can be handled later in this block"""
-                print(e)
+        df = pd.read_csv(u_path)
+        """Add new column 'bitCode', 'siCode' and add data to DataFrame"""
+        df = server.add_columns(df)
+        """Result Looks Like {('shC', 'C'): ['S1', 'S2', 'S4']}"""
+        source_dict = server.source_dict(json_input, df) # 
+        """Result Looks Like {(#G_code1#, #G_code2#, #sourceK#): [data rows]}"""
+        result_list = server.filter_data(source_dict, df)
+        
+        server.ask_output(source_dict, result_list)
+        return
+    except Exception as e:
+        """exceptions can be handled later in this block"""
+        print(e)
+    
+def csv_sql_data(json_input):
+    try:
+        u_path = input("Enter the path and filename of press Enter if 'db_table.csv'-> ") # open the dialog
+        # 'db_table.csv' by default
+        u_path = 'db_table.csv' if u_path == '' else u_path
+        """Validating json_input by regular expression."""
+        json_input = server.check_json(json_input)
+
+        df = pd.read_csv(u_path)
+        """Add new column 'bitCode', 'siCode' and add data to DataFrame"""
+        df = server.add_columns_sql(df)
+        """Result Looks Like {('shC', 'C'): ['S1', 'S2', 'S4']}"""
+        source_dict = server.source_dict_sql(json_input, df) # 
+        """Result Looks Like {(#G_code1#, #G_code2#, #sourceK#): [data rows]}"""
+        result_list = server.filter_data_sql(source_dict, df)
+        
+        server.ask_output(source_dict, result_list)
+        return
+
+    except Exception as e:
+            """exceptions can be handled later in this block"""
+            print(e)
 
 
 def test():
-    csv_data(json_input)
+    csv_sql_data(json_input)
 
 
 if __name__ == '__main__':
--- SGM/server.py
+++ SGM/server.py
@@ -2,41 +2,63 @@
 # Python 3.8.1
 import json
 import pandas as pd
-from typing import Dict, NamedTuple, Optional
+import pandasql as ps
 import re
 
 import exceptions
 
-class Input(Dict):
-    """Structure input"""
-    code1: str
-    code2: str
-    G_code1: str
-    G_code2: str
+class InputJson():
+    """Structure input. On the input:dict -> string atributes returns on the output
+
+    code1: str	(first key of input:dict)
+    
+    code2: str	(second key of input:dict)
+    
+    G_code1: str	(first value of input:dict)
+    
+    G_code2: str	(second value of input:dict)
+    
+    json: dict	(original dictionary json)
+    """
+    def __init__(self, json_input:dict) -> str:
+        self.code1 = str(list(json_input.keys())[0])
+        self.code2 = str(list(json_input.keys())[1])
+        self.G_code1 = str(list(json_input.values())[0])
+        self.G_code2 = str(list(json_input.values())[1])
+        self.json = json_input
 
 
-class Output(Dict):
-    """Structure output"""
-    G_code1: str
-    G_code2: str
-    source: str
-    raw_data: list
+class SourcesDict():
+    """Structure output. On the input:dict -> atributes    
+    
+    G_code1: str	(first value of input:dict)
+    
+    G_code2: str	(second value of input:dict)
+    source_list: List   (list of sources)
+    json: dict	(original dictionary json)
+    """
+    def __init__(self,dict_input):
+        self.G_code1 = str(list(dict_input.keys())[0][0])
+        self.G_code2 = str(list(dict_input.keys())[0][1])
+        self.G_code_tuple = list(dict_input.keys())[0]
+        self.source_list = list(dict_input.values())[0]
+        self.G_tuple = (self.G_code1, self.G_code2)
+        self.json = dict_input
 
-def check_json(json_input: dict)-> dict:
+
+def check_json(json_input: dict)-> InputJson:
     """Validating json_input by regular expression."""
     """For example below:"""
     """{"code1": "shA", "code2": "W"}"""
-    """{"code1": "shB", "code2": "W"}"""
-    if type(json_input) == list:
-        [check_json(json) for json in json_input]
-    else:    
-        pattern = r"\{(\"|\')(code1){1}(\"|\'):\s+(\"|\')(sh)[A-Z](\"|\'),\s+(\"|\')(code2)(\"|\'):\s+(\"|\')[A-Z](\"|\')\}"
-        json_str = json.dumps(json_input)
-        regexp_result = re.match(pattern, json_str)
-        if not regexp_result:
-            raise exceptions.NotCorrectJsonMessage(
-                "Not Correct format JSON input, please check.")
-        print("Validating json_input is OK ...")
+    """{"code1": "shB", "code2": "W"}""" 
+    pattern = r"\{(\"|\')(code1){1}(\"|\'):\s+(\"|\')(sh)[A-Z](\"|\'),\s+(\"|\')(code2)(\"|\'):\s+(\"|\')[A-Z](\"|\')\}"
+    json_str = json.dumps(json_input)
+    regexp_result = re.match(pattern, json_str)
+    if not regexp_result:
+        raise exceptions.NotCorrectJsonMessage(
+            "Not Correct format JSON input, please check.")
+    print("Validating json_input is OK ...")
+    return InputJson(json_input)
 
 def add_columns(df: pd.DataFrame) ->pd.DataFrame:
     """Add new column 'bitCode' and data to DataFrame"""
@@ -65,56 +87,122 @@ def add_columns(df: pd.DataFrame) ->pd.DataFrame:
     print("Add new column 'siCode' and data is OK ...")
     return df
 
-def filter_data(input_df: pd.DataFrame, df: pd.DataFrame) -> dict:
-    """Selects data by two steps"""
-    """First step - select data from code1 and code2"""
+def source_dict(json_input: InputJson, df: pd.DataFrame) -> dict:
     """First step result like {('shC', 'C'): ['S1', 'S2', 'S4']}"""
+
+    filter_df = df[(df['code1'].str[:3] == json_input.G_code1) & (df['code2'].str[:1] == json_input.G_code2)]
+    key = (json_input.G_code1, json_input.G_code2)
+    value = list(filter_df['source'].unique())     
+    source_dict = SourcesDict({key: value})
+    """Now we have source_dict looks like:
+    {('shC', 'C'): ['S1', 'S2', 'S4']}"""
+    return source_dict
+    
+def filter_data(source_dict: SourcesDict, df: pd.DataFrame) ->list:
     """Second step - filter data and get data frame"""
     """Result Looks Like {(#G_code1#, #G_code2#, #sourceK#): [data rows]}"""
-    
-    #i=0
-    filter_list = []
     result_list = []
-    for i in range (input_df.shape[0]):
-        filter_df = df[(df['code1'].str[:3] == input_df['code1'][i]) & (df['code2'].str[:1] == input_df['code2'][i])]
-    #   filter_list.append({(input_df['code1'][i], input_df['code2'][i]): (list(filter_df['source'].unique()), filter_df['source'].shape[0])})
-        key = (input_df['code1'][i], input_df['code2'][i])
-        value = list(filter_df['source'].unique())     
-        filter_list.append({key: value})
-        """Now filter_list looks like:
-        [{('shC', 'C'): ['S1', 'S2', 'S4']}]"""
-        
-        result = {}
+    if len(source_dict.source_list) > 0:
+        for j in source_dict.source_list:
+            raws_key = source_dict.G_tuple + (j,)
+            raws_data = df[(df['code1'].str[:3] == source_dict.G_code1) & (df['code2'].str[:1] == source_dict.G_code2) & (df['source'] == j)]
+            value_raws = raws_data[ ['updateDate', 'code1', 'code2', 'code3', 'value', 'bitCode', 'siCode'] ].values.tolist()
+            result_list.append({raws_key: value_raws})
+        print("Data filtered successfully ...")
+        return result_list
+    else:
+        raise exceptions.DataFrameError(
+            "Сan't filter the Data, please check your DataFrame input.")
 
-        if len(value) > 0:
-            for j in value:
-                raws_key = key + (j,)
-                raws_data = df[(df['code1'].str[:3] == input_df['code1'][i]) & (df['code2'].str[:1] == input_df['code2'][i]) & (df['source'] == j)]
-                value_raws = raws_data[ ['updateDate', 'code1', 'code2', 'code3', 'value', 'bitCode', 'siCode'] ].values.tolist()
-                if result == {}:
-                    result = {raws_key: value_raws}
-                else:
-                    result_list.append(result)
-            print("Data filtered successfully ...")
-            return result if result_list == [] else result_list
-        else:
-            raise exceptions.DataFrameError(
-                "Сan't filter the Data, please check your DataFrame input.")
-            
-def ask_output(result):
+def ask_output(source_dict:SourcesDict, result_list:list):
     print("Do you need export JSON file ?")
     #print("Or press the enter to print the output.json")
-    u_path_output = input("Enter the path and filename of press Enter if 'output.json'-> ")
-    u_path_output = 'output.json' if u_path_output == '' else u_path_output
+    u_path = input("press Enter to write file -> 'list_of_sources.json' and 'data_source_N.json'")
+    u_path_output = 'list_of_sources.json'
+    
     with open(u_path_output, 'w') as f:
         try:
             with open(u_path_output,'w') as out:
-                # I think there's a way more JSON. but I haven't looked yet.
-
-                for key,value in result.items():
-                    out.write(f"{{{key}:{value}}}\n")
+                # write file 'list_of_sources.json'
+                out.write(f"{source_dict.json}")
                 print(f"File '{u_path_output}' was write successfully ...")
-
         except Exception as e:
-                """exceptions can be handled later in this block"""
-                print(e)
+            """exceptions can be handled later in this block"""
+            print(e)
+        
+    for index,source in enumerate(source_dict.source_list):
+        u_path_output = f"data_source_{source}.json"
+        with open(u_path_output, 'w') as f:
+            try:
+                with open(u_path_output,'w') as out:
+                    # write file 'data_source_N.json'
+                    out.write(f"{result_list[index]}")
+                    print(f"File 'data_source_{source}.json' was write successfully ...")
+
+            except Exception as e:
+                    """exceptions can be handled later in this block"""
+                    print(e)
+
+def add_columns_sql(df: pd.DataFrame) ->pd.DataFrame:
+    # use SQL 
+    add_columns_query = '''
+        SELECT *,
+            CASE source WHEN 'S1' THEN '1' ELSE '0'
+            END as bitCode,
+            CASE
+                WHEN source = 'S1' AND code3 IN ('AP', 'AH') THEN 'A'
+                WHEN source = 'S1' AND code3 = 'PRD' THEN 'B'
+                WHEN source = 'S1' AND code3 = 'YLD' THEN 'BpA'
+                WHEN source <> 'S1' AND code3 IN ('AP', 'AH') THEN 'H'
+                WHEN source <> 'S1' AND code3 = 'PRD' THEN 'T'
+                WHEN source <> 'S1' AND code3 = 'YLD' THEN 'TpH'
+                ELSE 'None'
+            END as siCode
+        FROM df;
+        '''
+    df = ps.sqldf(add_columns_query, globals())
+    return df
+
+def source_dict_sql(json_input: InputJson, df: pd.DataFrame) -> dict:
+    """Selects data"""
+    """First step - select data from code1 and code2"""
+
+    filter_sql_query = f'''
+    SELECT DISTINCT (source)
+    FROM df
+    WHERE code1 LIKE '{json_input.G_code1}%' AND code2 LIKE '{json_input.G_code2}%';
+    '''
+    unique_source = ps.sqldf(filter_sql_query, globals())
+    key = (json_input.G_code1, json_input.G_code2) # ('shC', 'C')
+    value = unique_source['source'].tolist() # ['S1', 'S2', 'S4']
+    source_dict = SourcesDict({key: value})
+    """Now we have source_dict looks like:
+    {('shC', 'C'): ['S1', 'S2', 'S4']}"""
+    return source_dict
+
+def filter_data_sql(source_dict: SourcesDict, df: pd.DataFrame) ->list:
+    """Second step - filter data and get data frame"""
+    """Result Looks Like {(#G_code1#, #G_code2#, #sourceK#): [data rows]}"""
+    
+    result = {}
+    result_list = []
+
+    if len(source_dict.source_list) > 0:
+        for source in source_dict.source_list:
+            raws_key = source_dict.G_tuple + (source,)
+            filter_query = f'''
+            SELECT updateDate, code1, code2, code3, value, bitCode, siCode
+            FROM df
+            WHERE code1 LIKE '{source_dict.G_code1}%' AND code2 LIKE '{source_dict.G_code2}%' AND source = {source};
+            '''
+            raw_data = ps.sqldf(filter_query, globals())
+            value_raws = raw_data.values.tolist()
+            if result == {}:
+                result = {raws_key: value_raws}
+            else:
+                result_list.append(result)
+        print("Data filtered successfully ...")
+        return result if result_list == [] else result_list
+    else:
+        raise exceptions.DataFrameError(
+            "Сan't filter the Data, please check your DataFrame input.")
